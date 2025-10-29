f643fe1634585f31002086059d0730cf00ae9bf8
Aaron Wisdom <aaronwisdom43@gmail.com>
2025-10-29 13:54:19 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/**/*.{html,twig,php}", "./**/*.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
